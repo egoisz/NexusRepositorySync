5359c70018bc298571b2ae5f22d886096f2d56bb
admin <244655802@qq.com>
2023-12-28 10:53:13 +0800

    初版

--- main.go
+++ main.go
@@ -1,99 +1,309 @@
 package main
 
 import (
-	"database/sql"
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"io"
+	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 	"time"
 )
 
 type NexusRequest struct {
-	Items []Items `json:"items"`
+	Items []Item `json:"items"`
 }
+
+type Item struct {
+	ID         string  `json:"id"`
+	Repository string  `json:"repository"`
+	Format     string  `json:"format"`
+	Group      string  `json:"group"`
+	Name       string  `json:"name"`
+	Version    string  `json:"version"`
+	Assets     []Asset `json:"assets"`
+}
+
+type Asset struct {
+	DownloadURL    string      `json:"downloadUrl"`
+	Path           string      `json:"path"`
+	ID             string      `json:"id"`
+	Repository     string      `json:"repository"`
+	Format         string      `json:"format"`
+	Checksum       Checksum    `json:"checksum"`
+	ContentType    string      `json:"contentType"`
+	LastModified   string      `json:"lastModified"`
+	LastDownloaded string      `json:"lastDownloaded"`
+	Uploader       string      `json:"uploader"`
+	UploaderIP     string      `json:"uploaderIp"`
+	FileSize       int64       `json:"fileSize"`
+	BlobCreated    string      `json:"blobCreated"`
+	Maven2         Maven2Class `json:"maven2"`
+}
+
 type Checksum struct {
 	Sha1   string `json:"sha1"`
 	Sha512 string `json:"sha512"`
 	Sha256 string `json:"sha256"`
 	Md5    string `json:"md5"`
 }
-type Maven2 struct {
-	Extension  string `json:"extension"`
-	GroupID    string `json:"groupId"`
-	ArtifactID string `json:"artifactId"`
-	Version    string `json:"version"`
-}
-type Items struct {
-	DownloadURL    string    `json:"downloadUrl"`
-	Path           string    `json:"path"`
-	ID             string    `json:"id"`
-	Repository     string    `json:"repository"`
-	Format         string    `json:"format"`
-	Checksum       Checksum  `json:"checksum"`
-	ContentType    string    `json:"contentType"`
-	LastModified   time.Time `json:"lastModified"`
-	LastDownloaded time.Time `json:"lastDownloaded"`
-	Uploader       string    `json:"uploader"`
-	UploaderIP     string    `json:"uploaderIp"`
-	FileSize       int       `json:"fileSize"`
-	BlobCreated    time.Time `json:"blobCreated"`
-	Maven2         Maven2    `json:"maven2"`
+
+type Maven2Class struct {
+	Extension  string  `json:"extension"`
+	GroupID    string  `json:"groupId"`
+	ArtifactID string  `json:"artifactId"`
+	Version    string  `json:"version"`
+	Classifier *string `json:"classifier,omitempty"`
 }
 
-func main() {
+type Nexus struct {
+	gorm.Model
+	DownloadURL    string
+	GroupID        string
+	ArtifactID     string
+	Version        string
+	Path           string
+	Extension      string
+	DownLoadStatus bool `gorm:"default:false"`
+	UpLoadStatus   bool `gorm:"default:false"`
+}
 
-	url := "http://172.30.86.46:18081/service/rest/v1/assets?repository=maven-public"
-	method := "GET"
+const DownLoadDir = "download"
 
-	client := &http.Client{}
-	req, err := http.NewRequest(method, url, nil)
+var DB = initDB()
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	req.Header.Add("accept", "application/json")
-	req.Header.Add("User-Agent", "Apifox/1.0.0 (https://apifox.com)")
+//var Log = initLog()
 
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer res.Body.Close()
-
-	//body, err := ioutil.ReadAll(res.Body)
-	var t NexusRequest
-	err = json.NewDecoder(res.Body).Decode(&t)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, v := range t.Items {
-		fmt.Println(v)
-	}
-
-	db, err := sql.Open("sqlite3", "database.db")
+func initDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("nexus.db"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS nexus (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			username TEXT,
-			email TEXT
-		)
-	`)
+	return db
+}
+
+func UploadNexusData() {
+	for {
+		time.Sleep(20 * time.Second)
+		log.Printf("开始上传文件至nexus\n")
+		var n []Nexus
+		DB.Where(
+			"down_load_status =? and up_load_status =?", true, false,
+		).Where(
+			"extension =? or extension=?", "pom", "jar").Find(&n)
+		for _, v := range n {
+			//fmt.Println(v.DownloadURL, v.DownLoadStatus, v.Extension, v.UpLoadStatus)
+			err := HttpPost(
+				v.GroupID,
+				v.ArtifactID,
+				v.Version,
+				path.Join(DownLoadDir, v.Path),
+				v.Extension)
+			if err != nil {
+				log.Printf("上传 %s 失败: %s\n", v.DownloadURL, err)
+				continue
+			}
+			DB.Where(Nexus{DownloadURL: v.DownloadURL}).Updates(Nexus{UpLoadStatus: true})
+			log.Printf("上传成功成功%s \n", v.DownloadURL)
+		}
+	}
+}
+
+func UpdateNexusData() {
+
+	url := "http://172.30.86.46:18081/service/rest/v1/components?repository=maven-public"
+	method := "GET"
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+	// 无线循环
+	for {
+		// 隔10秒请求一次
+		time.Sleep(10 * time.Second)
+		log.Printf("开始更新数据库\n")
+
+		req, err := http.NewRequest(method, url, nil)
+
+		if err != nil {
+			log.Printf("%s\n", err)
+			continue
+		}
+		req.Header.Add("accept", "application/json")
+
+		res, err := client.Do(req)
+		if err != nil {
+			log.Printf("%s\n", err)
+			continue
+		}
+		defer res.Body.Close()
+		var t NexusRequest
+		err = json.NewDecoder(res.Body).Decode(&t)
+		if err != nil {
+			log.Printf("%s\n", err)
+		}
+		if err != nil {
+			println("failed to connect database")
+		}
+		//db.Create(&Nexus{DownloadURL: v.DownloadURL, GroupID: v.Maven2.GroupID, ArtifactID: v.Maven2.ArtifactID, Version: v.Maven2.Version})
+		//db.Create(&Nexus{DownloadURL: "v.DownloadURL"})
+
+		for _, item := range t.Items {
+			//fmt.Println(v.DownloadURL)
+			// 不存在URL则新建目录
+			for _, asset := range item.Assets {
+				DB.Where(Nexus{DownloadURL: asset.DownloadURL}).FirstOrCreate(&Nexus{
+					DownloadURL: asset.DownloadURL,
+					Path:        asset.Path,
+					GroupID:     asset.Maven2.GroupID,
+					ArtifactID:  asset.Maven2.ArtifactID,
+					Version:     asset.Maven2.Version,
+					Extension:   asset.Maven2.Extension,
+				})
+				//err := HttpGet(v.DownloadURL, v.Path)
+				if err != nil {
+					log.Printf("%s\n", err)
+				}
+			}
+
+		}
+		log.Printf("数据库更新完成\n")
+	}
+}
+
+func HttpPost(groupId string,
+	artifactId string,
+	version string,
+	filePath string,
+	extension string,
+) error {
+	url := "http://172.30.86.46:18081/service/rest/v1/components?repository=test-upload"
+	method := "POST"
+
+	payload := &bytes.Buffer{}
+	writer := multipart.NewWriter(payload)
+	_ = writer.WriteField("maven2.groupId", groupId)
+	_ = writer.WriteField("maven2.artifactId", artifactId)
+	_ = writer.WriteField("maven2.version", version)
+	file, errFile4 := os.Open(filePath)
+	defer file.Close()
+	part4, errFile4 := writer.CreateFormFile("maven2.asset1", filepath.Base(filePath))
+	_, errFile4 = io.Copy(part4, file)
+	if errFile4 != nil {
+		//fmt.Println(errFile4)
+		return errFile4
+	}
+	_ = writer.WriteField("maven2.asset1.extension", extension)
+	err := writer.Close()
 	if err != nil {
-		log.Fatal(err)
+		//fmt.Println(err)
+		return err
 	}
 
-	defer db.Close()
-	//if err != nil {
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+	req, err := http.NewRequest(method, url, payload)
 
-	//	fmt.Println(err)
-	//	return
-	//}
-	//fmt.Println(string(body))
+	if err != nil {
+		//fmt.Println(err)
+		return err
+	}
+	req.Header.Add("accept", " application/json")
+	req.Header.Add("User-Agent", "Apifox/1.0.0 (https://apifox.com)")
+	req.Header.Add("Content-Type", "multipart/form-data")
+	req.Header.Add("Authorization", "Basic YWRtaW46SHlkZXZAbmV4dXMyMDIz")
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := client.Do(req)
+	if err != nil {
+		//fmt.Println(err)
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		//fmt.Println(err)
+		return err
+	}
+	if string(body) != "" {
+		//fmt.Println(err)
+		log.Printf("return body: %s\n", string(body))
+
+	}
+	return nil
+}
+
+func HttpGet(url string, pa string) error {
+
+	fileName := path.Base(url)
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	// 获得get请求响应的reader对象
+	reader := bufio.NewReaderSize(res.Body, 32*1024)
+	filePath := path.Join(DownLoadDir, path.Dir(pa))
+
+	err = os.MkdirAll(filePath, 755)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(path.Join(filePath, fileName))
+	if err != nil {
+		return err
+	}
+	// 获得文件的writer对象
+	writer := bufio.NewWriter(file)
+
+	written, _ := io.Copy(writer, reader)
+	log.Printf("下载完成%s %s, Total length: %d \n", url, pa, written)
+	return nil
+}
+func init() {
+	os.Mkdir(DownLoadDir, 755)
+}
+
+func main() {
+	DB.AutoMigrate(&Nexus{})
+
+	//HttpPost("com.aliyun",
+	//	"artifactId",
+	//	"4.0.3",
+	//	"download/com/aliyun/aliyun-java-sdk-core/4.0.3/aliyun-java-sdk-core-4.0.3.pom",
+	//	"pom")
+
+	// 上传nexus仓库
+	go UploadNexusData()
+	// 更新数据库
+	go UpdateNexusData()
+
+	// 下载http文件
+	for {
+		var n []Nexus
+		DB.Where("down_load_status =?", false).Find(&n)
+		//fmt.Println(n)
+		for _, v := range n {
+			fmt.Println(v.DownloadURL, v.DownLoadStatus)
+			err := HttpGet(v.DownloadURL, v.Path)
+			if err != nil {
+				fmt.Printf("下载失败%s\n", v.DownloadURL)
+			}
+			DB.Where(Nexus{DownloadURL: v.DownloadURL}).Updates(Nexus{DownLoadStatus: true})
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+	// time.Sleep(100 * time.Second)
 }
